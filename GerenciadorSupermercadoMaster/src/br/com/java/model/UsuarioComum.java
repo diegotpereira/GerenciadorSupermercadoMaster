e302ac90573d564eaec2556d642e6cd36d2153c0
T.I <T.I@TI.rsatacadao.local>
2020-10-19 11:59:33 -0300

    criação do arquivo classe UsuarioComum.java

---
+++ GerenciadorSupermercadoMaster/src/br/com/java/model/UsuarioComum.java
@@ -0,0 +1,17 @@
+package br.com.java.model;
+
+public class UsuarioComum extends Usuario {
+
+	public UsuarioComum() {
+		
+		// TODO Auto-generated constructor stub
+	}
+
+	public UsuarioComum(String nome, String cpf) {
+		super(nome, cpf);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
