ffaa80587a68ea5b1b456c990d7bedf5bdc84e16
T.I <T.I@TI.rsatacadao.local>
2020-10-19 10:19:46 -0300

    alteração no arquivo classe Usuario.java adicionado componentes e metodos

--- GerenciadorSupermercadoMaster/src/br/com/java/model/Usuario.java
+++ GerenciadorSupermercadoMaster/src/br/com/java/model/Usuario.java
@@ -1,5 +1,34 @@
 package br.com.java.model;
 
 public class Usuario {
+	
+	public String nome;
+	public String cpf;
+	
+	public Usuario() {}
+
+	public Usuario(String nome, String cpf) {
+		super();
+		this.nome = nome;
+		this.cpf = cpf;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	
 
 }
