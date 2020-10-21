5a909c9688ec4d300c2303bd983d542fbe5411f7
T.I <T.I@TI.rsatacadao.local>
2020-10-21 11:05:03 -0300

    alteração no arquivo classe ProdutoDAO.java modificação na adição dos atributos e metodos get e set movimentados da classe produtos, verificar

--- GerenciadorSupermercadoMaster/src/br/com/java/dao/ProdutoDAO.java
+++ GerenciadorSupermercadoMaster/src/br/com/java/dao/ProdutoDAO.java
@@ -12,6 +12,45 @@ import br.com.java.model.Produtos;
 
 public class ProdutoDAO extends Produtos{
 	
+	private String codigo;
+	private String descricao;
+	private String medida;
+	private String valor;
+	
+	
+	
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getMedida() {
+		return medida;
+	}
+
+	public void setMedida(String medida) {
+		this.medida = medida;
+	}
+
+	public String getValor() {
+		return valor;
+	}
+
+	public void setValor(String valor) {
+		this.valor = valor;
+	}
+
 	public boolean salvar(String codigo, String telaDescricao, String medidaTela, String valorTela, String caminho) {
 		if (codigo.isEmpty()) {
 			String descricao = telaDescricao;
